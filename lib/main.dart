ca75164a7e8d6888eef8a26783190a6b7e0daff5
Tegar04 <you@example.comlux.arca2004@gmail.com>
2024-10-29 01:33:25 +0700

    notif

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:project_praktikum_mobile_kel3/app/modules/notification/notification_controller.dart';
 import 'package:project_praktikum_mobile_kel3/app/modules/welcome/view/welcome_page.dart';
 import './app/routes/app_pages.dart'; // Pastikan path ini sesuai dengan struktur folder Anda
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(); // Inisialisasi Firebase
+  Get.put(NotificationController()); // Register NotificationController
   runApp(const MyApp());
 }
 
